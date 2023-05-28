d36af3ddf20d23d053fb3997489ad81f0805812b
Gabriel Mota <ggabeti@gmail.com>
2023-05-28 20:43:30 -0300

    pizza list with razor pages

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-using ContosoPizza.Data;
-using ContosoPizza.Services;
-
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<PizzaContext>(options =>
-    options.UseSqlite("Data Source=ContosoPizza.db"));
 builder.Services.AddScoped<PizzaService>();
 
 var app = builder.Build();
